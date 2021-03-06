a8fa862cabe18d80ab262a8a05e59bb73296631f
Zulkar Nine <zulkarnine43@gmail.com>
2021-03-06 15:31:42 +0600

    some node js code here

---
+++ Lesson1/student.js
@@ -0,0 +1,19 @@
+exports.getName = () =>{
+    return "Zulkar Nine";
+}
+
+const getAge = () => {
+    return "22";
+}
+
+const cgpa = 3.40;
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = cgpa;
+
+// module.exports = {
+//     getName,
+//     getAge,
+//     cgpa
+// }
